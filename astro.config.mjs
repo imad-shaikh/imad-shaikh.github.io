70356ff8aa4b4e1921f91af2771e4a4c520feff4
Imad Shaikh <imsanity9129@gmail.com>
2026-05-25 22:17:41 -0400

    Disable sitemap during deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://imad-shaikh.github.io',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx()],
 });
